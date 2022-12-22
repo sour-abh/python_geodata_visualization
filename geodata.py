c2a73a5f3ee1c0acfd0f8021072999a1e0b43b07
Sourabh kumar <54701904+sour-abh@users.noreply.github.com>
2022-12-22 17:26:19 +0530

    added geodata.py
    geodata contains the code to extract places data using google map api with the help of sqlite and urllib

---
+++ geodata.py
@@ -0,0 +1,75 @@
+import urllib.request, urllib.parse, urllib.error
+import http
+import sqlite3
+import json
+import time
+import ssl
+import sys
+
+api_key = False
+
+if api_key is False:
+  api_key=42
+  serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else:
+  serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+#Additional detail for urllib
+#http.client.HTTPConnection.debuglevel=1
+
+conn = sqlite3.connect('geodata.sqlite')
+cur = conn.cursor()
+
+cur.execute(
+  ''' CREATE TABLE IF NOT EXISTS Locations(address TEXT,geodata TEXT)''')
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+fh = open("where.data")
+count = 0
+for line in fh:
+  if count > 200:
+    print("retrieved 200 locations,restart to retreive more")
+    break
+  address = line.strip()
+  print('')
+  cur.execute("SELECT geodata FROM Locations WHERE address = ?",
+              (memoryview(address.encode()), ))
+  try:
+    data = cur.fetchone()[0]
+    print("Found in database ", address)
+    continue
+  except:
+    pass
+  parms = dict()
+  parms['address'] = address
+  if api_key is not False: parms['key'] = api_key
+  url = serviceurl + urllib.parse.urlencode(parms)
+
+  print("Retreiving", url)
+  uh = urllib.request.urlopen(url, context=ctx)
+  data = uh.read().decode()
+  print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
+  count = count + 1
+  try:
+    js = json.loads(data)
+  except:
+    print(data)
+    continue
+
+  if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS'):
+    print('===========Faliure To Retrive ===========')
+    print(data)
+    break
+  cur.execute(''' INSERT INTO Locations(address,geodata) VALUES (?,?)''',
+              (memoryview(address.encode()), memoryview(data.encode()) ))
+
+  conn.commit()
+
+  if count % 10 == 0:
+    print('Pausing for a bit....')
+    time.sleep(5)
+
+print(
+  'Run geodump.py to read the data from the database so you can visualize it')
